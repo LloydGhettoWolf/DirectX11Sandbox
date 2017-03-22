be592bd08b4c8ecd7fb1d53f8687b4cb19448ea4
LloydGhettoWolf <lloyd.crawley@gmail.com>
2017-03-22 23:03:50 +0000

    added bounding box and render to mesh

--- DirectX11Engine/SimpleMesh.cpp
+++ DirectX11Engine/SimpleMesh.cpp
@@ -6,9 +6,8 @@ bool SimpleMesh::Init(ID3D11Device* device,ProcessedMeshData* mesh)
 	if (!InitBuffers(device, mesh))
 		return false;
 
-	//Init Textures
-	//if (!InitTextures(device, mesh))
-		//return false;
+	if (!InitBoundingBox(device))
+		return false;
 
 	return true;
 }
@@ -73,25 +72,90 @@ bool SimpleMesh::InitBuffers(ID3D11Device* device, ProcessedMeshData* mesh)
 
 	mMaterialIndex = mesh->materialIndex;
 
+	min = mesh->min;
+	max = mesh->max;
+
 	return true;
 }
 
-bool SimpleMesh::InitTextures(ID3D11Device* device, ProcessedMeshData* mesh)
+bool SimpleMesh::InitBoundingBox(ID3D11Device* device)
 {
-	/*unsigned int numTextures = mesh->numTextures;
-	mTexture = new Texture[numTextures];
+	D3D11_SUBRESOURCE_DATA vertData, indexData;
+	HRESULT result;
 
-	for (int i = 0; i < numTextures; i++)
+	D3D11_BUFFER_DESC* vertBufferDesc = new D3D11_BUFFER_DESC;
+	D3D11_BUFFER_DESC* indexBufferDesc = new D3D11_BUFFER_DESC;
+
+
+	vertBufferDesc->Usage = D3D11_USAGE_DEFAULT;
+	vertBufferDesc->ByteWidth = sizeof(XMFLOAT4) * 8; //aabb has 8 points
+	vertBufferDesc->BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertBufferDesc->CPUAccessFlags = 0;
+	vertBufferDesc->MiscFlags = 0;
+	vertBufferDesc->StructureByteStride = 0;
+
+	XMFLOAT4 aabbVerts[8] =
 	{
-		string name = mesh->textureNames[i];
-		wstring wName(name.begin(), name.end());
+		//top
+		XMFLOAT4(min.x, min.y, min.z, 1.0f),
+		XMFLOAT4(max.x, min.y, min.z, 1.0f),
+		XMFLOAT4(max.x, min.y, max.z, 1.0f),
+		XMFLOAT4(min.x, min.y, max.z, 1.0f),
 
-		if (!mTexture[i].Init(device, &wName[0]))
-			return false;
-	}*/
+		//bottom
+		XMFLOAT4(min.x, max.y, min.z, 1.0f),
+		XMFLOAT4(max.x, max.y, min.z, 1.0f),
+		XMFLOAT4(max.x, max.y, max.z, 1.0f),
+		XMFLOAT4(min.x, max.y, max.z, 1.0f),
+	};
+
+
+	// Give the subresource structure a pointer to the vertex data.
+	vertData.pSysMem = &aabbVerts[0];
+	vertData.SysMemPitch = 0;
+	vertData.SysMemSlicePitch = 0;
+
+	// Now create the vertex buffer.
+	result = device->CreateBuffer(vertBufferDesc, &vertData, &mAABBVertBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	unsigned int indices[24] =
+	{
+		//top
+		0,1, 1,2, 2,3, 3,0,
+		//bottom
+		4,5, 5,6, 6,7, 7,4,
+
+		//sides
+		0,4, 1,5, 2,6, 3,7
+	};
+
+	// Set up the description of the static index buffer.
+	indexBufferDesc->Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc->ByteWidth = sizeof(unsigned int) * 24;
+	indexBufferDesc->BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc->CPUAccessFlags = 0;
+	indexBufferDesc->MiscFlags = 0;
+	indexBufferDesc->StructureByteStride = 0;
+
+	// Give the subresource structure a pointer to the index data.
+	indexData.pSysMem = &indices[0];
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	// Create the index buffer.
+	result = device->CreateBuffer(indexBufferDesc, &indexData, &mAABBIndexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
 
 	return true;
 }
+
 
 void SimpleMesh::Render(ID3D11DeviceContext* deviceContext)
 {
@@ -115,6 +179,28 @@ void SimpleMesh::Render(ID3D11DeviceContext* deviceContext)
 	deviceContext->DrawIndexed(mIndexCount, 0, 0);
 	
 	return;
+}
+
+void SimpleMesh::RenderBoundingBox(ID3D11DeviceContext* deviceContext)
+{
+	unsigned int offset;
+	unsigned int stride;
+	offset = 0;
+
+	// Set vertex buffer stride and offset.
+	stride = sizeof(XMFLOAT4);
+
+	// Set the vertex buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetVertexBuffers(0, 1, &mAABBVertBuffer, &stride, &offset);
+
+	// Set the index buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetIndexBuffer(mAABBIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+
+	//now we're set up, let's render!
+	deviceContext->DrawIndexed(24, 0, 0);
 }
 
 void SimpleMesh::ShutdownBuffers()
--- DirectX11Engine/SimpleMesh.h
+++ DirectX11Engine/SimpleMesh.h
@@ -19,6 +19,7 @@ public:
 	bool Init(ID3D11Device* device, ProcessedMeshData* mesh);
 	void Shutdown();
 	void Render(ID3D11DeviceContext* context);
+	void RenderBoundingBox(ID3D11DeviceContext* context);
 
 	int GetIndexCount( ) const { return mIndexCount; }
 	int GetMaterialIndex() const { return mMaterialIndex; }
@@ -26,15 +27,26 @@ public:
 	ID3D11Buffer* GetVertBuffer()  { return mVertBuffer; }
 	ID3D11Buffer* GetIndexBuffer()  { return mIndexBuffer; }
 
+	XMFLOAT4 GetBoxMin() const { return min; }
+	XMFLOAT4 GetBoxMax() const { return max; }
+
 private:
+	bool InitBoundingBox(ID3D11Device* device);
 	bool InitBuffers(ID3D11Device* device, ProcessedMeshData* mesh);
 	bool InitTextures(ID3D11Device* device, ProcessedMeshData* mesh);
 	void ShutdownBuffers();
 
 	ID3D11Buffer* mVertBuffer;
 	ID3D11Buffer* mIndexBuffer;
+
+	ID3D11Buffer* mAABBVertBuffer;
+	ID3D11Buffer* mAABBIndexBuffer;
+
 	DefaultDiffuseShader* shader;
 	int mMaterialIndex;
 	int mVertCount;
 	int mIndexCount;
+
+	XMFLOAT4 min;
+	XMFLOAT4 max;
 };
