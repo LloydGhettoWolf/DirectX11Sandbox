9c4eac574ac1307be355375a683c07273eb3c0b1
LloydGhettoWolf <lloyd.crawley@gmail.com>
2017-03-24 21:38:08 +0000

    forgot to include file in last commit

--- DirectX11Engine/DefaultDiffuseShader.cpp
+++ DirectX11Engine/DefaultDiffuseShader.cpp
@@ -6,94 +6,11 @@
 
 bool DefaultDiffuseShader::Init(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename, D3D11_INPUT_ELEMENT_DESC* inputs, int numElems)
 {
-	HRESULT result;
-
-	ID3D10Blob* errorMessage;
-	ID3D10Blob* vertexShaderBuffer;
-	ID3D10Blob* pixelShaderBuffer;
-	unsigned int numElements;
 	D3D11_BUFFER_DESC matrixBufferDesc;
 	D3D11_BUFFER_DESC lightDesc;
 	D3D11_BUFFER_DESC materialDesc;
 
-
-	// Initialize the pointers this function will use to null.
-	errorMessage = 0;
-	vertexShaderBuffer = 0;
-	pixelShaderBuffer = 0;
-
-	// Compile the vertex shader code.
-	result = D3DX11CompileFromFile(vsFilename, NULL, NULL, "main", "vs_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, NULL,
-		&vertexShaderBuffer, &errorMessage, NULL);
-
-	if (result != S_OK)
-	{
-		// If the shader failed to compile it should have writen something to the error message.
-		if (errorMessage)
-		{
-			OutputShaderErrorMessage(errorMessage, hwnd, vsFilename);
-		}
-		// If there was nothing in the error message then it simply could not find the shader file itself.
-		else
-		{
-			MessageBox(hwnd, vsFilename, L"Missing Shader File", MB_OK);
-		}
-
-		return false;
-	}
-
-	// Compile the pixel shader code.
-	result = D3DX11CompileFromFile(psFilename, NULL, NULL, "main", "ps_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, NULL,
-		&pixelShaderBuffer, &errorMessage, NULL);
-
-	if (result != S_OK)
-	{
-		// If the shader failed to compile it should have writen something to the error message.
-		if (errorMessage)
-		{
-			OutputShaderErrorMessage(errorMessage, hwnd, psFilename);
-		}
-		// If there was  nothing in the error message then it simply could not find the file itself.
-		else
-		{
-			MessageBox(hwnd, psFilename, L"Missing Shader File", MB_OK);
-		}
-
-		return false;
-	}
-
-	// Create the vertex shader from the buffer.
-	result = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), NULL, &mVertexShader);
-
-	if (result != S_OK)
-	{
-		return false;
-	}
-
-	// Create the pixel shader from the buffer.
-	result = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), pixelShaderBuffer->GetBufferSize(), NULL, &mPixelShader);
-
-	if (result != S_OK)
-	{
-		return false;
-	}
-
-
-	// Create the vertex input layout.
-	result = device->CreateInputLayout(inputs, numElems, vertexShaderBuffer->GetBufferPointer(),
-		vertexShaderBuffer->GetBufferSize(), &mLayout);
-
-	if (result != S_OK)
-	{
-		return false;
-	}
-
-	// Release the vertex shader buffer and pixel shader buffer since they are no longer needed.
-	vertexShaderBuffer->Release();
-	vertexShaderBuffer = 0;
-
-	pixelShaderBuffer->Release();
-	pixelShaderBuffer = 0;
+	Shader::Init(device, hwnd, vsFilename, psFilename, inputs, numElems);
 
 	// Setup the description of the dynamic matrix constant buffer that is in the vertex shader.
 	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
@@ -104,7 +21,7 @@ bool DefaultDiffuseShader::Init(ID3D11Device* device, HWND hwnd, WCHAR* vsFilena
 	matrixBufferDesc.StructureByteStride = 0;
 
 	// Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class.
-	result = device->CreateBuffer(&matrixBufferDesc, NULL, &mMatrixBuffer);
+	HRESULT result = device->CreateBuffer(&matrixBufferDesc, NULL, &mMatrixBuffer);
 
 	if (result != S_OK)
 	{
@@ -144,8 +61,10 @@ bool DefaultDiffuseShader::Init(ID3D11Device* device, HWND hwnd, WCHAR* vsFilena
 	return true;
 }
 
-void DefaultDiffuseShader::ShutdownShader()
+void DefaultDiffuseShader::Shutdown()
 {
+	Shader::Shutdown();
+
 	// Release the matrix constant buffer.
 	if (mMatrixBuffer)
 	{
@@ -162,91 +81,28 @@ void DefaultDiffuseShader::ShutdownShader()
 		mMaterialBuffer->Release();
 	}
 
-	// Release the layout.
-	if (mLayout)
-	{
-		mLayout->Release();
-	}
-
-	// Release the pixel shader.
-	if (mPixelShader)
-	{
-		mPixelShader->Release();
-	}
-
-	// Release the vertex shader.
-	if (mVertexShader)
-	{
-		mVertexShader->Release();
-	}
-
 	return;
 }
 
-void DefaultDiffuseShader::Shutdown()
-{
-	// Shutdown the vertex and pixel shaders as well as the related objects.
-	ShutdownShader();
-
-	return;
-}
 
 bool DefaultDiffuseShader::PrepareShader(ID3D11DeviceContext* deviceContext)
 {
-	// Now render the prepared buffers with the shader.
-	// Set the vertex input layout.
-	deviceContext->IASetInputLayout(mLayout);
-
-	// Set the vertex and pixel shaders that will be used to render this triangle.
-	deviceContext->VSSetShader(mVertexShader, NULL, 0);
-	deviceContext->PSSetShader(mPixelShader, NULL, 0);
-
+	Shader::PrepareShader(deviceContext);
 	return true;
 }
 
 
-void DefaultDiffuseShader::OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, WCHAR* shaderFilename)
-{
-	char* compileErrors;
-	unsigned long bufferSize, i;
-	std::ofstream fout;
-
-
-	// Get a pointer to the error message text buffer.
-	compileErrors = (char*)(errorMessage->GetBufferPointer());
-
-	// Get the length of the message.
-	bufferSize = errorMessage->GetBufferSize();
-
-	// Open a file to write the error message to.
-	fout.open("shader-error.txt");
-
-	// Write out the error message.
-	for (i = 0; i<bufferSize; i++)
-	{
-		fout << compileErrors[i];
-	}
-
-	// Close the file.
-	fout.close();
-
-	// Release the error message.
-	errorMessage->Release();
-	errorMessage = 0;
-
-	// Pop a message up on the screen to notify the user to check the text file for compile errors.
-	MessageBox(hwnd, L"Error compiling shader.  Check shader-error.txt for message.", shaderFilename, MB_OK);
-
-	return;
-}
-
-bool DefaultDiffuseShader::SetConstantShaderParameters(ID3D11DeviceContext* deviceContext, MatrixBufferType *matrices, LightPosBuffer* lights)
+bool DefaultDiffuseShader::SetConstantShaderParameters(void* data, ID3D11DeviceContext* deviceContext)
 {
 	D3D11_MAPPED_SUBRESOURCE mappedResource;
 	D3D11_MAPPED_SUBRESOURCE mappedResource2;
 	
 	MatrixBufferType* matPtr;
 	LightPosBuffer* lightPtr;
+
+	ConstantsStruct* constants = static_cast<ConstantsStruct*>(data);
+	MatrixBufferType* matrices = constants->matPtr;
+	LightPosBuffer* lights = constants->lightPtr;
 
 	// Lock the constant buffer so it can be written to.
 	HRESULT result = deviceContext->Map(mMatrixBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
@@ -287,13 +143,13 @@ bool DefaultDiffuseShader::SetConstantShaderParameters(ID3D11DeviceContext* devi
 	return true;
 }
 
-bool DefaultDiffuseShader::SetPerMeshParamaters(void* data, ID3D11DeviceContext* deviceContext) 
+bool DefaultDiffuseShader::SetPerMeshParameters(void* data, ID3D11DeviceContext* deviceContext) 
 {
 	D3D11_MAPPED_SUBRESOURCE mappedResource;
 	MaterialProperties* meshPtr3;
 
 	//unpack data
-	PerMeshStruct* info = (PerMeshStruct*)data;
+	DiffuseShaderPerMeshStruct* info = static_cast<DiffuseShaderPerMeshStruct*>(data);
 	MaterialProperties* matInfo = info->material;
 	ID3D11SamplerState* samplerState = info->sampler;
 	ID3D11ShaderResourceView* srv = info->srv;
