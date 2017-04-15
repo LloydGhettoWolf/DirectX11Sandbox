003e13f9ffc0d094cebaf916ba5da9fc4dbdcfae
LloydGhettoWolf <lloyd.crawley@gmail.com>
2017-04-15 12:55:25 +0100

    added point sprite experiment to test app

--- TestProject/TestApp.cpp
+++ TestProject/TestApp.cpp
@@ -16,8 +16,11 @@
 #include "NormalMapShader.h"
 #include "NormNoSpecMapShader.h"
 #include "AlphaMaskShader.h"
+#include "BasicPointSpriteShader.h"
 #include <Renderer.h>
 #include <ResourceAllocator.h>
+#include <PointSprite.h>
+#include <algorithm>
 
 #define DIRECTINPUT_VERSION 0x0800
 
@@ -44,6 +47,7 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 	}
 
 	mResourceAllocator = new ResourceAllocator(mD3D->GetDevice());
+	mRenderer = new Renderer(mD3D->GetDevice(), mD3D->GetDeviceContext());
 
 	// Initialize the model object.
 
@@ -52,12 +56,11 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 	string strName(name.begin(), name.end());
 	string strPath(path.begin(), path.end());
 
-	mRenderer = new Renderer(mD3D->GetDevice(), mD3D->GetDeviceContext());
-
 	ProcessedMeshData* meshData;
 	string* textureNames;
 	ProcessedNormalMappedMeshData* mappedMeshData;
-	ReadBoomFile(strPath, strName, mNumMeshes, mNumMappedMeshes, mNumMaterials, mNumTextures, &textureNames, &meshData, &mMaterialProperties);
+	materialInfo* materialProperties;
+	ReadBoomFile(strPath, strName, mNumMeshes, mNumMappedMeshes, mNumMaterials, mNumTextures, &textureNames, &meshData, &materialProperties);
 
 	// Create the model object.
 	mMesh = new SimpleMesh[mNumMeshes];
@@ -68,7 +71,7 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 
 	for (unsigned int i = 0; i < mNumMeshes; i++)
 	{
-		result = mMesh[i].Init(mD3D->GetDevice(), mResourceAllocator, &meshData[i]);
+		result = mMesh[i].Init(mResourceAllocator, &meshData[i]);
 
 		if (!result)
 		{
@@ -94,7 +97,6 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 			return false;
 		}
 	}
-
 	delete[] textureNames;
 
 	// Create the color shader object.
@@ -103,7 +105,6 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 	{
 		return false;
 	}
-
 
 	// Initialize the color shader object.
 	result = mMeshShaders[DIFF_SHADER]->Init(mD3D->GetDevice(), hwnd);
@@ -174,6 +175,24 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 		return false;
 	}
 
+	mMeshShaders[INSTANCE_SHADER] = new PSShader();
+	if (!mMeshShaders[INSTANCE_SHADER])
+	{
+		return false;
+	}
+
+	result = mMeshShaders[INSTANCE_SHADER]->Init(mD3D->GetDevice(), hwnd);
+	if (!result)
+	{
+		MessageBox(hwnd, "Could not initialize the point sprite shader object.", "Error", MB_OK);
+		return false;
+	}
+
+
+	//now meshes, textures and materials are created, we really should try and pair materials with shaders
+	PairMaterialsWithShaders(materialProperties);
+	delete[] materialProperties;
+
 	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 	XMVECTOR position = XMVectorSet(0.0f, 50.0f, 0.0f, 0.0f);
 	XMVECTOR lookAt = XMVectorSet(10.0f, 50.0f, 0.0f, 0.0f);
@@ -236,9 +255,63 @@ bool TestApp::Init(int screenWidth, int screenHeight, HWND hwnd, HINSTANCE hInst
 
 	mFrustum = new Frustum();
 
+	//create point sprite
+	XMFLOAT3 pos[4] = { XMFLOAT3(0.0f, 0.0f, 0.0f), 
+						XMFLOAT3(0.0f, 0.0f, 0.0f), 
+						XMFLOAT3(0.0f, 0.0f, 0.0f), 
+						XMFLOAT3(0.0f, 0.0f, 0.0f) };
+	mPointSprite = new PointSprite(1, &pos[0]);
+
+	if (!mPointSprite->Init(mResourceAllocator))
+	{
+		return false;
+	}
+
 	return true;
 }
 
+bool TestApp::PairMaterialsWithShaders(materialInfo* materials)
+{
+	mMaterials = new processedMaterialInfo[mNumMaterials];
+
+	for (unsigned int i = 0; i < mNumMaterials; i++)
+	{
+		materialInfo* info = &materials[i];
+
+		mMaterials[i].specular = materials[i].specular;
+		mMaterials[i].diffuse  = materials[i].diffuse;
+		
+		mMaterials[i].diffSrv = mTextures[info->diffTexIndex].GetTexture();
+
+		if (info->maskIndex != 0)
+		{
+			mMaterials[i].maskSrv = mTextures[info->maskIndex].GetTexture();
+			mMaterials[i].shader = mMeshShaders[MASK_SHADER];
+		}
+		else if (info->specTexIndex != 0 && info->normMapIndex != 0)
+		{
+			mMaterials[i].normSrv = mTextures[info->normMapIndex].GetTexture();
+			mMaterials[i].specSrv = mTextures[info->specTexIndex].GetTexture();
+			mMaterials[i].shader = mMeshShaders[NORMMAP_SHADER];
+		}
+		else if (info->normMapIndex != 0)
+		{
+			mMaterials[i].normSrv = mTextures[info->normMapIndex].GetTexture();
+			mMaterials[i].shader = mMeshShaders[NORM_NO_SPEC_SHADER];
+		}
+		else if (info->specTexIndex != 0)
+		{
+			mMaterials[i].specSrv = mTextures[info->specTexIndex].GetTexture();
+			mMaterials[i].shader = mMeshShaders[SPEC_SHADER];
+		}
+		else
+		{
+			mMaterials[i].shader = mMeshShaders[DIFF_SHADER];
+		}
+		
+	}
+	return true;
+}
 
 void TestApp::Shutdown()
 {
@@ -269,20 +342,20 @@ void TestApp::Shutdown()
 		delete mMeshShaders[3];
 	}
 
-	if (mOutlineShader)
+	if (mMeshShaders[4])
+	{
+		delete mMeshShaders[4];
+	}
+
+	/*if (mOutlineShader)
 	{
 		mOutlineShader->Shutdown();
 		delete mOutlineShader;
-	}
+	}*/
 
 	// Release the model object.
 	if (mMesh)
 	{
-		for (unsigned int i = 0; i < mNumMeshes; i++)
-		{
-			mMesh[i].Shutdown();
-		}
-
 		delete[] mMesh;
 	}
 
@@ -302,9 +375,14 @@ void TestApp::Shutdown()
 			mTextures[i].Shutdown();
 	}
 
-	if (mMaterialProperties)
+	if (mMaterials)
 	{
-		delete[] mMaterialProperties;
+		delete[] mMaterials;
+	}
+
+	if (mPointSprite)
+	{
+		delete mPointSprite;
 	}
 
 	return;
@@ -376,92 +454,67 @@ bool TestApp::Render()
 
 
 	vector<SimpleMesh*> unculledMeshes = CullMeshesAgainstFrustum(mMesh, mNumMeshes, mFrustum);
+	XMMATRIX projView = mCamera->GetProjection() * mCamera->GetView();
+	unsigned int numUnculledMeshes = (unsigned int)unculledMeshes.size();
+
+	for (unsigned int i = 0; i < numUnculledMeshes; i++)
+	{
+		UpdateThisFrameZ(unculledMeshes[i], &projView);
+	}
+	std::sort(unculledMeshes.begin(), unculledMeshes.end(), CompareZAscend);
+
+	vector<SimpleMesh*> alphaMeshes;
 
 	// Put the model vertex and index buffers on the graphics pipeline to prepare them for drawing.
-	unsigned int numUnculledMeshes = (unsigned int)unculledMeshes.size();
+
 	Shader* thisShader = nullptr;
 	ID3D11ShaderResourceView* diffSrv = nullptr;
 	ID3D11ShaderResourceView* specSrv = nullptr;
 	ID3D11ShaderResourceView* normSrv = nullptr;
 	ID3D11ShaderResourceView* alphaSrv = nullptr;
 
-	//for (unsigned int i = 0; i < numUnculledMeshes; i++)
-	for (unsigned int i = 0; i < mNumMeshes; i++)
-	{
-		// Render the model using the color shader.
-		int materialIndex = mMesh[i].GetMaterialIndex();//unculledMeshes[i]->GetMaterialIndex();
-		materialInfo material = mMaterialProperties[materialIndex];
-		if (material.maskIndex != 0)
-		{
-			diffSrv = mTextures[material.maskIndex].GetTexture();
-			alphaSrv = mTextures[material.maskIndex].GetTexture();
-			thisShader = mMeshShaders[MASK_SHADER];
-		}
-		else if (material.specTexIndex != 0 && material.normMapIndex != 0)
-		{
-			normSrv = mTextures[material.normMapIndex].GetTexture();
-			specSrv = mTextures[material.specTexIndex].GetTexture();
-			thisShader = mMeshShaders[NORMMAP_SHADER];
-		}
-		else if (material.normMapIndex != 0)
-		{
-			normSrv = mTextures[material.normMapIndex].GetTexture();
-			thisShader = mMeshShaders[NORM_NO_SPEC_SHADER];
-		}
-		else if (material.specTexIndex != 0)
-		{
-			specSrv = mTextures[material.specTexIndex].GetTexture();
-			thisShader = mMeshShaders[SPEC_SHADER];
-		}
-		else
-		{
-			thisShader = mMeshShaders[DIFF_SHADER];
-		}
-			
-		result = thisShader->PrepareShader(context);
+	for (unsigned int i = 0; i < unculledMeshes.size();)
+	{ 
+		int materialIndex = unculledMeshes[i]->GetMaterialIndex();
+		processedMaterialInfo* info = &mMaterials[materialIndex];
 
-		ConstantsStruct constants;
-		constants.lightPtr = &lights;
-		constants.matPtr = &matrices;
-		result = thisShader->SetConstantShaderParameters((void*)&constants, context);
-
-		MaterialProperties materialProperties;
-		materialProperties.diffuseCol = XMFLOAT4(material.diffuse.x, material.diffuse.y, material.diffuse.z, 1.0f);
-		materialProperties.specComponent = material.specFactor;
-
-		diffSrv = mTextures[material.diffTexIndex].GetTexture();
-
-		if (material.maskIndex != 0)
+		if (info->maskSrv != 0)
 		{
-			AlphaMaskShaderPerMeshStruct perMesh;
-			perMesh.material = &materialProperties;
-			perMesh.sampler = mSamplerState;
-			perMesh.diffuseSrv = diffSrv;
-			perMesh.alphaSrv = alphaSrv;
-
-			context->OMSetBlendState(mBlendState, 0, 0xffffffff);
-			thisShader->SetPerMeshParameters(static_cast<void*>(&perMesh), mD3D->GetDeviceContext());
-		}
-		else
-		{
-			NormShaderPerMeshStruct perMesh;
-			perMesh.material = &materialProperties;
-			perMesh.sampler = mSamplerState;
-			perMesh.diffuseSrv = diffSrv;
-			perMesh.specSrv = specSrv;
-			perMesh.normSrv = normSrv;
-
-			thisShader->SetPerMeshParameters(static_cast<void*>(&perMesh), mD3D->GetDeviceContext());
+			alphaMeshes.emplace_back(unculledMeshes[i]);
+			unculledMeshes.erase(unculledMeshes.begin() + i);
+			continue;
 		}
 
-		mRenderer->DrawIndexed(mMesh[i].GetVertBuffer(), mMesh[i].GetNumBuffers(), mMesh[i].GetStrides(), mMesh[i].GetIndexBuffer(), mMesh[i].GetIndexCount(), D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-		//unculledMeshes[i]->Render(context);
-		//mMesh[i].Render(context);
-
-		context->OMSetBlendState(NULL, 0, 0xffffffff);
+		i++;
 	}
 
+	RenderMeshList(unculledMeshes, &lights, &matrices);
+
+	unsigned int numAlphaMeshes = (unsigned int)alphaMeshes.size();
+	for (unsigned int i = 0; i < numAlphaMeshes; i++)
+	{
+		UpdateThisFrameZ(alphaMeshes[i], &projView);
+	}
+	std::sort(alphaMeshes.begin(), alphaMeshes.end(), CompareZDescend);
+
+	context->OMSetBlendState(mBlendState, 0, 0xffffffff);
+	
+	//RenderMeshList(unculledMeshes, &lights, &matrices);
+
+	context->OMSetBlendState(NULL, 0, 0xffffffff);
+
+	//set shader for point sprite
+	mMeshShaders[INSTANCE_SHADER]->PrepareShader(mD3D->GetDeviceContext());
+	ConstantsStruct constants;
+	constants.lightPtr = &lights;
+	constants.matPtr = &matrices;
+	result = mMeshShaders[INSTANCE_SHADER]->SetConstantShaderParameters((void*)&constants, context);
+
+	ID3D11Buffer* pointVerts = mPointSprite->GetVertBuffer();
+	ID3D11Buffer* instanceData = mPointSprite->GetInstanceBuffer();
+	ID3D11Buffer* vertBuffers[2] = { pointVerts, instanceData };
+	unsigned int strides[2] = { sizeof(XMFLOAT3) , sizeof(XMFLOAT3) };
+	mRenderer->DrawIndexedInstanced(&vertBuffers[0], 2, strides, mPointSprite->GetIndexBuffer(), 6, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST, 1);
 
 #if RENDER_AABB
 	result = mOutlineShader->PrepareShader(context);
@@ -487,4 +540,46 @@ bool TestApp::Render()
 	mD3D->EndScene();
 
 	return true;
+}
+
+void TestApp::RenderMeshList(vector<SimpleMesh*>& meshes, LightPosBuffer* lights, MatrixBufferType* matrices)
+{
+	unsigned int numMeshesInList = meshes.size();
+	ID3D11DeviceContext* context = mD3D->GetDeviceContext();
+
+	for (unsigned int i = 0; i < numMeshesInList; i++)
+	{
+		// Render the model using the color shader.
+		int materialIndex = meshes[i]->GetMaterialIndex();
+		processedMaterialInfo* info = &mMaterials[materialIndex];
+
+		HRESULT result = info->shader->PrepareShader(context);
+
+		ConstantsStruct constants;
+		constants.lightPtr = lights;
+		constants.matPtr = matrices;
+		result = info->shader->SetConstantShaderParameters((void*)&constants, context);
+
+		MaterialProperties materialProperties;
+		materialProperties.diffuseCol = XMFLOAT4(info->diffuse.x, info->diffuse.y, info->diffuse.z, 1.0f);
+		materialProperties.specCol = XMFLOAT4(info->specular.x, info->specular.y, info->specular.z, 1.0f);
+		materialProperties.specComponent = 10.0f;
+
+		ShaderPerMeshStruct perMesh;
+		perMesh.material = &materialProperties;
+		perMesh.sampler = mSamplerState;
+		perMesh.diffuseSrv = info->diffSrv;
+		perMesh.specSrv = info->specSrv;
+		perMesh.maskSrv = info->maskSrv;
+		perMesh.normSrv = info->normSrv;
+
+		info->shader->SetPerMeshParameters(static_cast<void*>(&perMesh), mD3D->GetDeviceContext());
+
+		mRenderer->DrawIndexed(meshes[i]->GetVertBuffer(),
+			meshes[i]->GetNumBuffers(),
+			meshes[i]->GetStrides(),
+			meshes[i]->GetIndexBuffer(),
+			meshes[i]->GetIndexCount(),
+			D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	}
 }
--- TestProject/TestApp.h
+++ TestProject/TestApp.h
@@ -2,6 +2,7 @@
 //TestApp.h
 #include <BaseApp.h>
 #include <DirectXMath.h>
+#include <vector>
 
 class D3DClass;
 class SimpleMesh;
@@ -15,6 +16,10 @@ struct ID3D11BlendState;
 struct materialInfo;
 class Renderer;
 class ResourceAllocator;
+class PointSprite;
+struct LightPosBuffer;
+struct MatrixBufferType;
+struct processedMaterialInfo;
 
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 3000.0f;
@@ -26,8 +31,10 @@ const int SPEC_SHADER = 1;
 const int NORMMAP_SHADER = 2;
 const int NORM_NO_SPEC_SHADER = 3;
 const int MASK_SHADER = 4;
+const int INSTANCE_SHADER = 5;
 
 using namespace DirectX;
+using namespace std;
 
 class TestApp : public BaseApp
 {
@@ -41,19 +48,23 @@ public:
 	bool Frame(DIMOUSESTATE& state);
 
 private:
+	bool PairMaterialsWithShaders(materialInfo* materials);
 	bool Render();
+	void RenderMeshList(vector<SimpleMesh*>& meshes, LightPosBuffer* lights, MatrixBufferType* matrices);
 	bool ReadInput(DIMOUSESTATE& state);
 
 	SimpleMesh* mMesh;
+	PointSprite* mPointSprite;
+	
 	Renderer* mRenderer;
 	ResourceAllocator* mResourceAllocator;
 	
-	Shader* mMeshShaders[NORM_NO_SPEC_SHADER + 1];
+	Shader* mMeshShaders[INSTANCE_SHADER];
 	OutlineShader* mOutlineShader;
 	ID3D11SamplerState* mSamplerState;
 	ID3D11BlendState* mBlendState;
 
-	materialInfo* mMaterialProperties;
+	processedMaterialInfo* mMaterials;
 
 	Texture* mTextures;
 
